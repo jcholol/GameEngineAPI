4423d2794bcba2a425478568714880bbf3028116
David Kim <kimdav011@gmail.com>
2020-03-06 15:40:07 -0800

    update

---
+++ KimChoGridApi/public_html/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+
+app.use(express.static('.'));
+
+app.listen(port, () => { console.log("Server Listening on port 8081") });
