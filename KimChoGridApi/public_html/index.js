d7500d1140391f0725e07b4105e5c2eb79a751d2
Jonathan Yunho Cho <chojon99@uw.edu>
2022-02-23 12:18:02 -0800

    updating nodejs

--- KimChoGridApi/public_html/index.js
+++ KimChoGridApi/public_html/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8081;
+const port = 3000;
 
 app.use(express.static('.'));
 
-app.listen(port, () => { console.log("Server Listening on port 8081"); });
+app.listen(port, () => { console.log("Server Listening on port 3000"); });
