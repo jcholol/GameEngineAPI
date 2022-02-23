efc26069e795fe37f96ac20d4f42fe805d564788
Jonathan Yunho Cho <chojon99@uw.edu>
2022-02-23 15:31:34 -0800

    testing

--- KimChoGridApi/public_html/app.js
+++ KimChoGridApi/public_html/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8081;
+const port = 8080;
 
 app.use(express.static('.'));
 
-app.listen(port, () => { console.log("server listening on port 8081"); });
+app.listen(port, () => { console.log("server listening on port 8080"); });
